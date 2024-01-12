101c0c8acc29b11826bc06f491d3eca981aaa25a
Patbob123 <flyingbikeddd@gmail.com>
2024-01-12 18:15:12 -0500

    Updates

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+import { FadeInSection } from './FadeinSection.js';
+export function Hero() {
+  return /*#__PURE__*/React.createElement("div", {
+    id: "home",
+    className: "flex"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "z-[2] absolute top-0 grid grid-cols-1 grid-rows-1 lg:grid-rows-1 lg:grid-cols-1 w-full"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "z-[2] h-[500px] flex flex-col justify-center items-center"
+  }, /*#__PURE__*/React.createElement(FadeInSection, null, /*#__PURE__*/React.createElement("div", {
+    className: "titlefont font-extrabold text-transparent text-center text-3xl sm:text-5xl lg:text-8xl bg-clip-text bg-gradient-to-r from-red-400 to-pink-600 duration-700"
+  }, "The Road")), /*#__PURE__*/React.createElement(FadeInSection, null, /*#__PURE__*/React.createElement("div", {
+    className: "text-slate-100 font-bold text-1xl sm:text-1xl lg:text-3xl"
+  }, "Cormac McCarthy")))));
+}
